f8af3525668827222c7687ea1650ba0faac31570
gunjot3547 <gunjot13547@gmail.com>
2025-03-18 13:39:02 +0530

    Added solution for Reverse a String

---
+++ questions/2025-03-18_Reverse_a_String.js
@@ -0,0 +1,5 @@
+/**
+     * Question: Reverse a String
+     * Description: A function to reverse a given string.
+     */
+    function reverseString(str) { return str.split('').reverse().join(''); }
